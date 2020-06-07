89ac5a011d7f742ddc343a7ccee26fb12e71bff3
mouhssine65 <mouhssine.kdidar@gmail.com>
2020-06-07 13:26:13 +0200

    code coverage + test report

---
+++ src/test/java/BankTransactionTest.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class BankTransactionTest {
+
+    String message = "Hello World";
+
+    @Test
+    public void testPrintMessage() {
+        assertEquals(message,"Hello World");
+    }
+
+}
